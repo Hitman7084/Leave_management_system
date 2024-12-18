9b8f563727e295e6045abaf2137dc3779bdd0496
Himanshu Mall <hitman7084@gmail.com>
2024-12-18 15:14:21 +0530

    Update urls.py

--- Leave_mgt_sys/clms/urls.py
+++ Leave_mgt_sys/clms/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from .import views
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.homepage, name='homepage'),
+    path('professor/', views.professor_dashboard, name='professor_dashboard'),
+    path('incharge/', views.incharge_dashboard, name='incharge_dashboard'),
+    path('dean/', views.dean_dashboard, name='dean_dashboard'),
 ]
+
