89c951c343c9c82c706f9523e6bbcf2b77895683
Himanshu Mall <hitman7084@gmail.com>
2024-12-18 15:28:54 +0530

    styles.css

--- Leave_mgt_sys/clmsapp/views.py
+++ Leave_mgt_sys/clmsapp/views.py
@@ -2,15 +2,5 @@ from django.shortcuts import render
 
 from django.shortcuts import render
 
-def homepage(request):
-    return render(request, 'index.html')
 
-def professor_dashboard(request):
-    return render(request, 'professor_dashboard.html')
-
-def incharge_dashboard(request):
-    return render(request, 'incharge_dashboard.html')
-
-def dean_dashboard(request):
-    return render(request, 'dean_dashboard.html')
 
