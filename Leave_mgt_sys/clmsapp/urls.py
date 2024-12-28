172b7e7d60b527d04c8fbc403f51ec59c69b7c9e
Himanshu Mall <hitman7084@gmail.com>
2024-12-29 03:19:46 +0530

    urls added in clmsapp

--- Leave_mgt_sys/clmsapp/urls.py
+++ Leave_mgt_sys/clmsapp/urls.py
@@ -1,2 +1,11 @@
 from django.conf import settings
+from django.urls import path
+from . import views
 
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('verify_email/', views.verify_email, name='verify_email'),
+    path('forgot_password/', views.forgot_password, name='forgot_password'),
+    path('reset_password/', views.reset_password, name='reset_password'),
+]
