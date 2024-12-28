100e98262013ecf5dff0ad689ac9ad36480ff8ba
Himanshu Mall <hitman7084@gmail.com>
2024-12-29 02:32:48 +0530

    Email_backend

--- Leave_mgt_sys/clmsapp/models.py
+++ Leave_mgt_sys/clmsapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class User(AbstractUser):
+    ROLES = [
+        ('Professor', 'Professor'),
+        ('Dean', 'Dean'),
+        ('Incharge', 'Incharge'),
+        ('Student', 'Student'),
+    ]
+    full_name = models.CharField(max_length=100)
+    photo = models.ImahgeField(upload_to='photos/', null=True, blank=True)
 # Create your models here.
