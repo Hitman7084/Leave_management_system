8cae4ebef39d54266e2b0d849eb7750f6acfeddc
Himanshu Mall <hitman7084@gmail.com>
2024-12-29 02:59:02 +0530

    Registration done

--- Leave_mgt_sys/clmsapp/views.py
+++ Leave_mgt_sys/clmsapp/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.contrib import messages
+from .forms import RegistrationForm, ForgotPasswordForm
+from .models import User
+import random
 
-from django.shortcuts import render
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False                # Account not created until verification is done
+            user.save()
 
+            # Otp generation
+            otp = random.randint(10000, 99999)
+            request.session['otp'] = otp
+            request.session['email'] = user.email
 
-
+            send_mail(
+                'Account Verification',
+                f'Your OTP is {otp}. It is valid for 5 minutes.',
+                'cliad350@gmail.com',
+                [user.email],
+                fail_silently=False,
+            )
+            messages.success(request, 'Account created successfully. Please verify your email.')
+            return redirect('verify_email')
+        else:
+            form = RegistrationForm()
+        return render(request, 'register.html', {'form': form})
