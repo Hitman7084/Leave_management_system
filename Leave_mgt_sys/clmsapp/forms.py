53eee9e1e9119f8213238f2834972a01aee5493e
Himanshu Mall <hitman7084@gmail.com>
2024-12-29 02:40:06 +0530

    forms application

--- Leave_mgt_sys/clmsapp/forms.py
+++ Leave_mgt_sys/clmsapp/forms.py
@@ -2,3 +2,10 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    photo = forms.ImageField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'full_name', 'photo', 'email', 'password', 'rtpassword', 'role']
