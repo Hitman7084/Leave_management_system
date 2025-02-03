130842f92e54e6a0d8fb526ffc4aeca9b11fc963
Himanshu Mall <hitman7084@gmail.com>
2025-02-03 20:56:06 +0530

    styling for forms

--- Leave_mgt_sys/clmsapp/forms.py
+++ Leave_mgt_sys/clmsapp/forms.py
@@ -3,12 +3,19 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    photo = forms.ImageField(required=True)
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-register'}))
+    photo = forms.ImageField(required=True, widget=forms.FileInput(attrs={'class': 'form-register'}))
 
     class Meta:
         model = User
         fields = ['username', 'full_name', 'photo', 'email', 'password1', 'password2', 'role']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-register'}),
+            'full_name': forms.TextInput(attrs={'class': 'form-register'}),
+            'role': forms.Select(attrs={'class': 'form-register'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-register'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-register'}),
+        }
 
 class ForgotPasswordForm(forms.Form):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-register'}))
