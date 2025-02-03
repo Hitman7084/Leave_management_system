b208b113884a5be9e69db5bdd0688bc341a04955
Himanshu Mall <hitman7084@gmail.com>
2025-02-03 20:13:48 +0530

    authenticate code discarded
    Didn't work

--- Leave_mgt_sys/clms/views.py
+++ Leave_mgt_sys/clms/views.py
@@ -7,28 +7,3 @@ def homepage(request):
 
 def login(request):
     return render(request, 'login.html')
-
-'''def authenticate_user(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        # authentication logic lagana hai yha
-        if username == 'admin' and password == 'aspirin':  # Example 
-            return render(request, 'authenticate_user.html', {
-                'authenticated': True,
-                'username': username,
-            })
-    return render(request, 'authenticate_user.html', {'authenticated': False})
-
-def professor_dashboard(request):
-    return render(request, 'professor_dashboard.html')
-
-def incharge_dashboard(request):
-    return render(request, 'incharge_dashboard.html')
-
-def dean_dashboard(request):
-    return render(request, 'dean_dashboard.html')
-
-def student_dashboard(request):
-    return render(request, 'student_dashboard.html')'''
-
--- Leave_mgt_sys/clmsapp/models.py
+++ Leave_mgt_sys/clmsapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+import random
 
 class User(AbstractUser):
     ROLES = [
@@ -14,4 +16,18 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
 # Create your models here.
+
+class OTPVerification(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    otp = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField(null=True, blank=True)
+    verified = models.BooleanField(default=False)
+
+    def generate_otp(self):
+        self.otp = str(random.randint(100000, 999999))
+        self.expires_at = timezone.now() + timezone.timedelta(minutes=10)  # Set OTP expiry
+        self.verified = False
+        self.save()
