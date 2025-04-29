0eac63b262530ebf7d579da45ff342e0b479bbed
Himanshu Mall <62309305+Hitman7084@users.noreply.github.com>
2025-04-29 08:50:02 +0530

    pdf file fixed and downloadble now

--- Leave_mgt_sys/clms/settings.py
+++ Leave_mgt_sys/clms/settings.py
@@ -124,6 +124,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+MEDIA_URL = '/leave_attachments/'
+MEDIA_ROOT = BASE_DIR / 'leave_attachments'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- Leave_mgt_sys/clms/urls.py
+++ Leave_mgt_sys/clms/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -24,3 +26,6 @@ urlpatterns = [
     path('', include('clmsapp.urls')),
     path('logout/', auth_views.LogoutView.as_view(next_page='homepage'), name='logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
